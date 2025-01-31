804dae739dc1d0a1f16a2c39de08db1b18b33ae3
Olukemi Fasuan <ayanfe1711@gmail.com>
2025-01-31 17:52:41 +0100

    added admin routes

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const userRoutes = require('../routes/v1/userRoutes');
 const categoryRoutes = require('../routes/v1/categoryRoutes');
 const productRoutes = require('../routes/v1/productRoutes');
+const adminRoutes = require('../routes/v1/adminRoutes');
 
 const app = express();
 
@@ -11,5 +12,6 @@ app.use(express.json());
 app.use('/v1/users', userRoutes);
 app.use('/v1/categories', categoryRoutes);
 app.use('/v1/products', productRoutes);
+app.use('/v1/admin', adminRoutes);
 
 module.exports = app;
